4c87cfaa81204c50d0c15d0ebae67534aa1bdcf1
Rimfoer <polegoshkosergey@mail.ru>
2023-05-11 15:30:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,40 @@
-function timeConv(val, val2) { // function that converts frames into human-readable time
-	var time = Math.floor(val/val2), hours = Math.floor(time/3600), minutes = Math.floor((time-hours*3600)/60), seconds = time-(hours*3600+minutes*60), frames = val%val2;
-	if(hours <= 9) { hours = '0' + hours; }
-	if(minutes <= 9) { minutes = '0' + minutes; }
-	if(seconds <= 9) { seconds = '0' + seconds; }
-	if(frames <= 9 && val2 < 120) { frames = '0' + frames; }
-	if(frames <= 9 && val2 >= 120) {
-		frames = '00' + frames;
-	} else if(frames >= 10 && frames <= 99 && val2 >= 120) {
-		frames = '0' + frames;
+function getTime(numberFrames, frameRate, displayFormat) {
+	var frame = numberFrames || 2073599, fps = frameRate || 24, format = displayFormat || 'frames', time = Math.floor(frame/fps), hours = Math.floor(time/3600),
+	minutes = Math.floor((time-hours*3600)/60), seconds = time-(hours*3600+minutes*60), frames = frame%fps, milsecs = Math.floor(1000*(frame%fps)/fps);
+	if(frames < 10 && fps < 120 || frames >= 10 && frames < 100 && fps >= 120) { frames = `0${frames}`; } else if(frames < 10 && fps >= 120) { frames = `00${frames}`; }
+	if(milsecs < 10) { milsecs = `00${milsecs}`; } else if(milsecs >= 10 && milsecs < 100) { milsecs = `0${milsecs}`; } 
+	if(seconds < 10) { seconds = `0${seconds}`; }
+	if(minutes < 10) { minutes = `0${minutes}`; }
+	if(hours < 10) { hours = `0${hours}`; }
+	switch(format) {
+		case 'frames': { return `${hours}:${minutes}:${seconds}.${frames}`; break; }
+		case 'milsec': { return `${hours}:${minutes}:${seconds}.${milsecs}`; break; }
 	}
-	return hours + ':' + minutes + ':' + seconds + '.' + frames;
 }
 
-function getRandomInteger(min, max) { // random integer generator
-	return Math.floor(Math.random() * (max - min)) + min;
+function getRandomInteger(min, max) {
+	var minValue = min || 0, maxValue = max || 9;
+	return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
 }
 
-function getRandomArray(arr) { // generator of a random value obtained from an array
-	var rand = Math.random()*arr.length | 0;
+function getRandomArray(array) {
+	var arr = array || [12,34,56,78,90], rand = Math.random()*arr.length | 0;
 	return arr[rand];
 }
 
-$('#totalFrames').on('input', function() { // when the user enters a value in the input field, it is processed in real time by calling the timeConv() function
-	var val = $(this).val(), val2 = $('#frameRate').val();
-	if(val2 >= 100 && val >= 10000000) {
-		$('#totalFrames').css('width', '83px');
-	} else {
-		$('#totalFrames').removeAttr('style');
-	}
-	$('#resultField').val(timeConv(val, val2));
-});
-
-function inputLimiter(val) {
-	$('#totalFrames').attr('max', val);
+function inputLimiter(max, maxLength) {
+	var maxValue = max || 2073599, maxLengthValue = maxLength || 7; 
+	$('#numberFrames').attr({'max': maxValue, 'maxlength': maxLengthValue});
 }
 
-$('#frameRate').on('click', function() {
-	if($(this).val() == 24) {
-		inputLimiter(2073599);
-	} else if($(this).val() == 25) {
-		inputLimiter(2159999);
-	} else if($(this).val() == 30) {
-		inputLimiter(2591999);
-	} else if($(this).val() == 50) {
-		inputLimiter(4319999);
-	} else if($(this).val() == 60) {
-		inputLimiter(5183999);
-	} else if($(this).val() == 120) {
-		inputLimiter(10367999);
-	} else if($(this).val() == 240) {
-		inputLimiter(20735999);
+function changePlaceHolder(ph) {
+	switch(ph) {
+		case 0: { $('#resultField').prop('placeholder', '00:00:00.00'); break; }
+		case 1: { $('#resultField').prop('placeholder', '00:00:00.000'); break; }
 	}
+}
 
-	if($(this).val() >= 100) {
-		$('#resultField').prop('placeholder', '00:00:00.000');
-		$('#resultField').css('width', '85px');
-	} else {
-		$('#resultField').prop('placeholder', '00:00:00.00');
-		$('#resultField').removeAttr('style');
-	}
-	$('#resultField').val(timeConv($('#totalFrames').val(), $(this).val()));
-});
-
-$('.clearButton').click(function() { // no comments
-	$('#totalFrames, #frameRate, #resultField').val('');
-	$('#frameRate').val('24');
-	inputLimiter(2073599);
-});
-
-$('.exampleButton').click(function() { // no comments (x2)
-	var arr = [24, 25, 30, 50, 60, 120, 240], fps, fr = getRandomArray(arr);
-	if(fr == 24) {
-		fps = getRandomInteger(1, 2073599);
-	} else if(fr == 25) {
-		fps = getRandomInteger(1, 2159999);
-	} else if(fr == 30) {
-		fps = getRandomInteger(1, 2591999);
-	} else if(fr == 50) {
-		fps = getRandomInteger(1, 4319999);
-	} else if(fr == 60) {
-		fps = getRandomInteger(1, 5183999);
-	} else if(fr == 120) {
-		fps = getRandomInteger(1, 10367999);
-	} else if(fr == 240) {
-		fps = getRandomInteger(1, 20735999);
-	}
-
-	if(fr >= 120) {
-		$('#resultField').css('width', '85px');
-		if(fps >= 10000000) {
-			$('#totalFrames').css('width', '83px');
-		} else {
-			$('#totalFrames').removeAttr('style');
-		}
-	} else {
-		$('#totalFrames, #resultField').removeAttr('style');
-	}
-
-	$('#totalFrames').val(fps);
-	$('#frameRate').val(fr);
-	$('#resultField').val(timeConv(fps, fr));
-});
+function getFrameTime(frameRate) {
+	var fps = frameRate || 24, ft = 1000/fps, ms = ft.toString().substr(0,4);
+	$('#frameTime').val(`${ms} ms`);
+}
