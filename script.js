7bdfd1e0518d404984c148e91a3d6bce9127fd3c
Rimfoer <polegoshkosergey@mail.ru>
2023-05-12 17:25:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function getTime(numberFrames, frameRate, displayFormat) {
-	var frame = numberFrames || 2073599, fps = frameRate || 24, format = displayFormat || 'frames', time = Math.floor(frame/fps), hours = Math.floor(time/3600),
-	minutes = Math.floor((time-hours*3600)/60), seconds = time-(hours*3600+minutes*60), frames = frame%fps, milsecs = Math.floor(1000*(frame%fps)/fps);
-	if(frames < 10 && fps < 120 || frames >= 10 && frames < 100 && fps >= 120) { frames = `0${frames}`; } else if(frames < 10 && fps >= 120) { frames = `00${frames}`; }
-	if(milsecs < 10) { milsecs = `00${milsecs}`; } else if(milsecs >= 10 && milsecs < 100) { milsecs = `0${milsecs}`; } 
+	var frame = numberFrames || 1036799,
+	    fps = frameRate || 24,
+	    format = displayFormat || 'frames',
+	    time = Math.floor(frame/fps),
+	    hours = Math.floor(time/3600),
+	    minutes = Math.floor((time-hours*3600)/60),
+	    seconds = time-(hours*3600+minutes*60),
+	    frames = frame%fps,
+	    milsecs = Math.floor(1000*(frame%fps)/fps);
+	
+	if(frames < 10 && fps < 120 || frames >= 10 && frames < 100 && fps >= 120) {
+		frames = `0${frames}`;
+	} else if(frames < 10 && fps >= 120) {
+		frames = `00${frames}`;
+	}
+	if(milsecs < 10) {
+		milsecs = `00${milsecs}`;
+	} else if(milsecs >= 10 && milsecs < 100) {
+		milsecs = `0${milsecs}`;
+	} 
 	if(seconds < 10) { seconds = `0${seconds}`; }
 	if(minutes < 10) { minutes = `0${minutes}`; }
 	if(hours < 10) { hours = `0${hours}`; }
